f8e8f2b873f52a05a168a7c9f14fa7e5e264efe9
ojp13 <32867061+ojp13@users.noreply.github.com>
2022-07-26 17:49:36 +0100

    Fixed pagination bug. It was showing 'next' after an edit incorrectly

--- src/pages/Feed/Feed.js
+++ src/pages/Feed/Feed.js
@@ -263,12 +263,14 @@ class Feed extends Component {
         };
         this.setState(prevState => {
           let updatedPosts = [...prevState.posts];
+          let updatedTotalPosts = prevState.totalPosts;
           if (prevState.editPost) {
             const postIndex = prevState.posts.findIndex(
               p => p._id === prevState.editPost._id
             );
             updatedPosts[postIndex] = post;
           } else {
+            updatedTotalPosts += 1;
             updatedPosts.pop();
             updatedPosts.unshift(post);
           }
@@ -277,7 +279,7 @@ class Feed extends Component {
             isEditing: false,
             editPost: null,
             editLoading: false,
-            totalPosts: this.state.totalPosts + 1
+            totalPosts: updatedTotalPosts
           };
         });
       })
